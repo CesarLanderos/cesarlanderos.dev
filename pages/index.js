e1bd7224b4f8743368cef0548f32ea65c8a7df02
Cesar Landeros Delgado <cesar.landeros@wizeline.com>
2019-03-02 13:53:42 -0600

    base website

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <>
+      <div>Hi!</div>
+      <div>
+        Send me an email to
+        <a href="mailto:me@cesarlanderos.dev">me@cesarlanderos.dev</a>
+      </div>
+    </>
+  );
+}
+
+export default Home;

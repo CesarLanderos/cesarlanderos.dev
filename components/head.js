8334fb1edec3275b007485d412028ca8456f9bd7
Cesar Landeros Delgado <cesar.landeros@wizeline.com>
2019-03-04 02:45:08 -0600

    add browser title

--- components/head.js
+++ components/head.js
@@ -5,6 +5,7 @@ export default function Head() {
   return (
     <>
       <DocumentHead>
+        <title>César Landeros</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link
           href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Righteous"

cb27f6ce2eb23cfb5b8597d0b3e2d408bfe95f36
Cesar Landeros <me@cesarlanderos.dev>
2023-12-18 01:51:41 -0600

    Add global styles to the application

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import { Sono } from "next/font/google";
 import "bulma/css/bulma.css";
+import "../styles/globals.css";
 
 const sono = Sono({
   subsets: ["latin"],
@@ -11,7 +12,7 @@ const sono = Sono({
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <main className={`${sono.variable}`}>
+    <main className={`${sono.variable} font-mono`}>
       <Component {...pageProps} />
     </main>
   );
